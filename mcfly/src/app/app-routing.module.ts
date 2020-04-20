60e3069d38140e5ee2313be4ab3faceaaede6d92
Silas Guimarães Ribeiro <guimaraes.silas@hotmail.com>
2020-04-20 19:14:26 -0300

    navigation

--- mcfly/src/app/app-routing.module.ts
+++ mcfly/src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./views/home/home.component";
+import { ProductsComponent } from "./views/products/products.component";
+import { ProductCreateComponent } from "./components/products/product-create/product-create.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+  },
+  {
+    path: "products",
+    component: ProductsComponent,
+  },
+  {
+    path: "products/create",
+    component: ProductCreateComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- mcfly/src/app/app.module.ts
+++ mcfly/src/app/app.module.ts
@@ -1,20 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HeaderComponent } from "./components/template/header/header.component";
+
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { FooterComponent } from "./components/template/footer/footer.component";
+import { NavComponent } from "./components/template/nav/nav.component";
+
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { HomeComponent } from "./views/home/home.component";
+import { MatCardModule } from "@angular/material/card";
+import { ProductsComponent } from "./views/products/products.component";
+import { ProductCreateComponent } from './components/products/product-create/product-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    ProductsComponent,
+    ProductCreateComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
